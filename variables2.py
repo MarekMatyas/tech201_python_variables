a4e55b86f0e6d8248c211989e551e74bccf5ccba
MarekMatyas <matyasamarek@gmail.com>
2023-01-27 15:55:44 -0800

    Age and all

---
+++ variables2.py
@@ -0,0 +1,34 @@
+# Variables
+
+#variable_name = variable_data
+# commenting multiple line is to mark all and ctrl + /
+
+#
+# a = 1 #int
+# b = 2 # int
+# c = 3.5 # float
+# hi = "Hello world" # string
+#
+#
+# # print(a)
+#
+# # print(a + b)
+#
+# # type method
+# print(type(c))
+#  # Overwriting variables
+# print(a)
+# a = 4
+# # getting user input
+# print("Hi, what is your name?")
+# name = input()
+# print("Hi")
+# print(name)
+
+print("Hello there, what is your name ?")
+name = input()
+print("Hello" +" " + name + " " + "How old are you ?")
+age = input()
+print("When were you born? ")
+DOB = input()
+print("It was nice to meet you !!")
